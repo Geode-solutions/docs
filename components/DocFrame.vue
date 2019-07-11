e88ff3019aac1d16946ac821d4c1b671c6728970
Arnaud Botella <botella.arnaud@gmail.com>
2019-07-11 15:53:34 +0200

    add title and button

--- components/DocFrame.vue
+++ components/DocFrame.vue
@@ -1,16 +1,19 @@
 <template>
   <v-layout column fill-height>
     <v-flex text-xs-center>
+      <v-btn @click="goIndex">
+        Back to index
+      </v-btn>
+      <span class="display-1  ">{{ project }} documentation</span>
       <iframe 
         ref="docFrame"
         width="100%"
         style="border: none;"
         scrolling="no"
-        :src="'/docs/' + project + '/index.html'"/>
+        :src="page"/>
     </v-flex>
   </v-layout>
 </template>
-
 
 <script>
 export default {
@@ -21,10 +24,20 @@ export default {
       type: String
     }
   },
+  computed: {
+    page() {
+      return '/docs/' + this.project + '/genindex.html';
+    }
+  },
   mounted() {
     this.$refs.docFrame.addEventListener("load", ()=>{
       this.$refs.docFrame.style.height = this.$refs.docFrame.contentWindow.document.body.offsetHeight + "px";
     });
+  },
+  methods: {
+    goIndex() {
+      this.$refs.docFrame.src = this.page;
+    }
   }
 }
 </script>
