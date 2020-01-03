75e0352dc9c1c2f197e3d469d413d3eeaa560b86
BotellaA <arnaud.botella@geode-solutions.com>
2020-01-03 16:12:58 +0100

    minor improvements

--- components/Theme.vue
+++ components/Theme.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 class="display-1 ma-5">{{ title }}</h2>
     <v-row>
-    <v-col v-for="card in cards" :key="card.title" cols="4">
+    <v-col v-for="card in cards" :key="card.title" cols="5" lg="4">
       <v-card nuxt :to="card.route" style="height: 100%">
         <v-card-title primary-title class="headline">{{ card.title }}</v-card-title>
         <v-card-text>{{ card.descr }}</v-card-text>
