aaf9524d0644e1a0144693e09698a86ed1980656
panquez <pierre.anquez@geode-solutions.com>
2020-03-09 09:31:57 +0100

    fix(CI/CD): Update CI/CD platforms

--- pages/where-to-get-code.vue
+++ pages/where-to-get-code.vue
@@ -1,12 +1,8 @@
 <template>
   <v-layout column>
-    <h1 class="display-4 ma-5">
-      Where to download OpenGeode code
-    </h1>
+    <h1 class="display-4 ma-5">Where to download OpenGeode code</h1>
 
-    <h2 :class="section">
-      Download pre-compiled binaries
-    </h2>
+    <h2 :class="section">Download pre-compiled binaries</h2>
 
     <p :class="paragraph">
       We provide three pre-compiled binaries for OpenGeode.
@@ -19,10 +15,13 @@
 
     <ul :class="paragraph">
       <li>
-        <strong>OpenGeode-X.Y.Z-Darwin.tar.gz</strong> corresponds to the package generated on macOS Catalina (10.15) for OpenGeode X.Y.Z. release;
+        <strong>OpenGeode-X.Y.Z-darwin.tar.gz</strong> corresponds to the package generated on macOS Catalina (10.15) for OpenGeode X.Y.Z. release;
       </li>
       <li>
-        <strong>OpenGeode-X.Y.Z-Linux.tar.gz</strong> corresponds to the package generated on Ubuntu 18.04 for OpenGeode X.Y.Z. release;
+        <strong>OpenGeode-X.Y.Z-rhel.tar.gz</strong> corresponds to the package generated on CentOS 7 for OpenGeode X.Y.Z. release;
+      </li>
+      <li>
+        <strong>OpenGeode-X.Y.Z-ubuntu.tar.gz</strong> corresponds to the package generated on Ubuntu 18.04 for OpenGeode X.Y.Z. release;
       </li>
       <li>
         <strong>OpenGeode-X.Y.Z-win64.zip</strong> corresponds to the package generated on Windows Visual Studio 15 2017 for OpenGeode X.Y.Z. release.
@@ -34,14 +33,14 @@
       You can now use these pre-compiled binaries for your own projects.
     </p>
 
-    <h2 :class="section">
-      Download source code
-    </h2>
+    <h2 :class="section">Download source code</h2>
 
     <p :class="paragraph">
       On the release page, you can also download source code archives.
       Extract the archive, and then compile the source code. Let be guided
-      <a href="/how-to-compile">here</a>.
+      <a
+        href="/how-to-compile"
+      >here</a>.
     </p>
   </v-layout>
 </template>
