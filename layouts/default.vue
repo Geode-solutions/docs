6040bece6566e80aa6ce55c53579f09711e23b04
Pierre Anquez <32702237+panquez@users.noreply.github.com>
2020-03-11 16:06:52 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,7 +36,7 @@ export default {
         { divider: 'GUIDES' },
         { title: 'DataModel', to: '/datamodel' },
         { title: 'Meshes', to: '/meshes' },
-        { title: 'Attributes', to: '/datamodel' },
+        { title: 'Attributes', to: '/attributes' },
         { title: 'Supported file formats', to: '/formats' },
         { divider: 'DEVELOPER GUIDES' },
         { title: 'Compile OpenGeode', to: '/how-to-compile' },
