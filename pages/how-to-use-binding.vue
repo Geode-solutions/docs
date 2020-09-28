57745bebcfb3f087bc263c7e9c001c853920d16f
BotellaA <arnaud.botella@geode-solutions.com>
2020-09-28 12:05:34 +0200

    add pypi support

--- pages/how-to-use-binding.vue
+++ pages/how-to-use-binding.vue
@@ -9,7 +9,7 @@
     </h2>
 
     <h3 :class="subsection">
-      Python
+      Python - pip
     </h3>
 
     <p :class="paragraph">
@@ -18,78 +18,41 @@
 
     <ul :class="paragraph">
       <li>3.6 and 3.7 on Windows - Visual Studio 2017</li>
-      <li>3.6, 3.7 and 3.8 on Ubuntu 18.04</li>
-      <li>2.7 on RHEL 7 / CentOS 7</li>
+      <li>3.6, 3.7 and 3.8 on Linux</li>
       <li>3.6, 3.7 and 3.8 on macOS X Catalina (10.15)</li>
     </ul>
 
-    <p :class="paragraph">
-      Pre-compiled binaries on these platforms are also provided.
-      <a
-        href="/where-to-get-code"
-      >This page</a> describes where to download these binaries.
-    </p>
-
     <h2 :class="section">
-      Using OpenGeode bindings from release packages
+      Using OpenGeode bindings from PyPI
     </h2>
 
     <p :class="paragraph">
-      The use of OpenGeode bindings is simple.
-      It only need few steps depending on your platform.
+      The use of OpenGeode bindings very is simple.
     </p>
 
-    <h3 :class="subsection">
-      On Windows
-    </h3>
-
     <h4 :class="subsubsection">
-      Configuration
+      Installation
     </h4>
 
-    <ol :class="paragraph">
-      <li>The bindings are located in the bin directory of the package.</li>
-      <li>Add this path to the <code>PYHTONPATH</code> and the <code>PATH</code> environment variables.</li>
-    </ol>
+    <p :class="paragraph">
+      The bindings are located in <a href="https://pypi.org/project/OpenGeode-core/">PyPI</a> under the name <code>OpenGeode-core</code>.
+      Install it using pip: <code>pip install OpenGeode-core</code>.
+    </p>
 
     <h4 :class="subsubsection">
       Execution
     </h4>
 
     <ol :class="paragraph">
-      <li>Open a Python script or Jupyter notebook.</li>
-      <li>Import OpenGeode bindings, there is one binding for each C++ library. Example for the basic library: 
-        <code>import opengeode_py_basic as basic</code>.</li>
-      <li>Use any OpenGeode objects, methods and functions <code>point = basic.Point3D([0.1, 42, 54.9])</code></li>
+      <li>Open a Python script or a Jupyter notebook.</li>
+      <li>Import OpenGeode bindings: <code>import opengeode</code>.</li>
+      <li>Use any OpenGeode objects, methods and functions <code>point = opengeode.Point3D([0.1, 42, 54.9])</code></li>
     </ol>
 
     <p :class="paragraph">
       That's all! You are ready to work with OpenGeode bindings.
     </p>
 
-    <h3 :class="subsection">
-      On Linux and macOS
-    </h3>
-
-    <h4 :class="subsubsection">
-      Configuration
-    </h4>
-
-    <ol :class="paragraph">
-      <li>The bindings are located in the lib directory of the package.</li>
-      <li>Add this path to the <code>PYHTONPATH</code> and the <code>LD_LIBRARY_PATH</code> environment variables.</li>
-    </ol>
-
-    <h4 :class="subsubsection">
-      Execution
-    </h4>
-
-    <ol :class="paragraph">
-      <li>Open a Python script or Jupyter notebook.</li>
-      <li>Import OpenGeode bindings, there is one binding for each C++ library. Example for the basic library: 
-        <code>import opengeode_py_basic as basic</code>.</li>
-      <li>Use any OpenGeode objects, methods and functions <code>point = basic.Point3D([0.1, 42, 54.9])</code></li>
-    </ol> 
   </v-layout>
 </template>
 
